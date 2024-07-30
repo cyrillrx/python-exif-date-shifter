31cbbab42a8c2626a3cf43d519b0f8b8f6f3635a
Cyril Leroux <cyril0leroux@gmail.com>
2024-07-30 14:43:37 +0200

    Replace piexif with exiftool to handle both photo and video formats

--- shift_exif.py
+++ shift_exif.py
@@ -1,13 +1,13 @@
 import argparse
 import os
 from datetime import datetime, timedelta
+from typing import List
 
-from PIL import Image
-from piexif import load as load_exif, dump as dump_exif, ExifIFD
+import exiftool
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Adjust EXIF dates of images by a specified time shift.")
+    parser = argparse.ArgumentParser(description="Adjust EXIF dates of images and videos by a specified time shift.")
     parser.add_argument("path", type=str, help="Path to the file or directory")
     parser.add_argument("shift", type=int, help="Time shift in hours")
     args = parser.parse_args()
@@ -18,7 +18,7 @@ def main():
 
 def process_path(path: str, time_shift: timedelta):
     if os.path.isfile(path):
-        if is_image(path):
+        if is_media(path):
             shift_exif_datetime(path, time_shift)
     elif os.path.isdir(path):
         process_folder(path, time_shift)
@@ -29,45 +29,62 @@ def process_path(path: str, time_shift: timedelta):
 def process_folder(folder_path: str, time_shift: timedelta):
     for filename in os.listdir(folder_path):
         filepath = os.path.join(folder_path, filename)
-        if is_image(filepath):
+        if is_media(filepath):
             shift_exif_datetime(filepath, time_shift)
 
 
 def shift_exif_datetime(filepath: str, time_shift: timedelta):
     print(f"EXIF adjusting data for file {filepath}")
     try:
-        exif_dict = load_exif(filepath)
-        shift_exif_datetimes(exif_dict, time_shift)
+        with exiftool.ExifToolHelper() as et:
+            metadata = et.get_metadata(filepath)
 
-        exif_bytes = dump_exif(exif_dict)
-
-        image = Image.open(filepath)
-        image.save(filepath, exif=exif_bytes)
+            new_metadata = shift_exif_datetimes(metadata, time_shift)
+            if new_metadata:
+                et.execute(*[f"-{key}={value}" for key, value in new_metadata.items()], filepath)
 
         print(f"EXIF data adjusted for {filepath}")
     except Exception as e:
         print(f"Failed to adjust EXIF data for {filepath}: {e}")
 
 
-def shift_exif_datetimes(exif_dict: dict, time_shift: timedelta):
-    shift_exif_time_with_key(exif_dict, ExifIFD.DateTimeOriginal, time_shift)
-    shift_exif_time_with_key(exif_dict, ExifIFD.DateTimeDigitized, time_shift)
+def shift_exif_datetimes(metadata: List, time_shift: timedelta) -> dict:
+    print(f"shift_exif_datetimes metadata length: {len(metadata)}")
+
+    output_metadata = {}
+
+    for input_metadata in metadata:
+        add_if_exists(input_metadata, output_metadata, 'EXIF:DateTimeOriginal', time_shift)
+        add_if_exists(input_metadata, output_metadata, 'EXIF:DateTimeDigitized', time_shift)
+
+        add_if_exists(input_metadata, output_metadata, 'QuickTime:CreateDate', time_shift)
+        add_if_exists(input_metadata, output_metadata, 'QuickTime:ModifyDate', time_shift)
+        add_if_exists(input_metadata, output_metadata, 'QuickTime:TrackCreateDate', time_shift)
+        add_if_exists(input_metadata, output_metadata, 'QuickTime:TrackModifyDate', time_shift)
+
+    return output_metadata
 
 
-def shift_exif_time_with_key(exif_dict: dict, exif_key: int, time_shift: timedelta):
-    current_datetime_str: str = exif_dict["Exif"][exif_key].decode('utf-8')
-    print(f"- cur datetime: {current_datetime_str} exif key: {exif_key}")
+def add_if_exists(input_metadata: dict, output_metadata: dict, key: str, time_shift: timedelta):
+    if key in input_metadata:
+        output_metadata[key] = shift_exif_time(input_metadata, key, time_shift)
+
+
+def shift_exif_time(metadata: dict, key: str, time_shift: timedelta) -> str:
+    current_datetime_str = metadata[key]
+    print(f"- cur datetime: {current_datetime_str} - key: {key}")
+
     current_datetime = datetime.strptime(current_datetime_str, '%Y:%m:%d %H:%M:%S')
 
     new_datetime = current_datetime + time_shift
 
     new_datetime_str: str = new_datetime.strftime('%Y:%m:%d %H:%M:%S')
-    print(f"- new datetime: {new_datetime_str} exif key: {exif_key}")
-    exif_dict["Exif"][exif_key] = new_datetime_str.encode('utf-8')
+    print(f"- new datetime: {new_datetime_str}")
+    return new_datetime_str
 
 
-def is_image(filepath: str):
-    return filepath.lower().endswith(('.jpg', '.jpeg', '.tiff', '.png'))
+def is_media(filepath: str):
+    return filepath.lower().endswith(('.jpg', '.jpeg', '.tiff', '.png', '.mp4', '.mov'))
 
 
 if __name__ == "__main__":
